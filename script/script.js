86c8580c6867a26347e58f2196c43a52fad58c7b
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-01-24 21:39:17 +0530

    Added update JSON code

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const xlsx = require("xlsx");
+const path = require("path");
 
 const excelFilePath = "../excel_data/Test Metadata.xlsx";
 const folderPath = "../sample_files/";
+const folderPathNewMeta = "../new_metadata_files";
 
 function renameFile(oldName, newName) {
   fs.rename(oldName, newName, (err) => {
@@ -30,3 +32,27 @@ function renameFilesFromExcel(excelFilePath, folderPath) {
 }
 
 renameFilesFromExcel(excelFilePath, folderPath);
+
+let i = 0;
+function updateJsonData(folderPath) {
+  const files = fs.readdirSync(folderPath);
+  files.forEach((file) => {
+    if (path.extname(file) === ".json") {
+      const filePath = path.join(folderPath, file);
+      const jsonData = JSON.parse(fs.readFileSync(filePath, "utf8"));
+      const nftName = path.parse(file).name;
+      if (jsonData["name"] !== ` `) {
+        jsonData["name"] = `NFT #${nftName}`;
+        jsonData.symbol = "NFT";
+        jsonData.collection = "NFT";
+        jsonData["image"] = `${nftName}.jpg`;
+        jsonData["description"] = `Say hello to my NFT`;
+      }
+
+      fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), "utf8");
+      console.log(`Updated ${file}, Total No.of files ${i++}`);
+    }
+  });
+}
+
+updateJsonData(folderPathNewMeta);
